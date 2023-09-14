4557e509a5952368fa3d068ac7cd9b92121e4e10
Admin <admin@MacBook-Air-Admin.local>
2023-09-14 18:01:20 +0200

    feature home

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
-
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-
-import { NavMenu } from "./components/navigation/NavMenu";
-import Box from "@mui/material/Box";
-import { appWrapper } from "./styles/styles";
-import { ContentArea } from "./components/contentArea/ContentArea";
+import { Home } from "./Pages/Home";
 
 function App() {
-  const [openSide, setOpenSide] = useState(false);
-
-  const handleSideToogle = () => {
-    setOpenSide(!openSide);
-  };
-
-  return (
-    <Box sx={appWrapper}>
-      <NavMenu handleSideToggle={handleSideToogle} />
-      <ContentArea open={openSide} />
-    </Box>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,19 @@
+import { ContentArea } from "../components/contentArea/ContentArea";
+import { NavMenu } from "../components/navigation/NavMenu";
+import { Box } from "@mui/material";
+import { appWrapper } from "../styles/styles";
+import { useState } from "react";
+
+export const Home = () => {
+  const [openSide, setOpenSide] = useState(false);
+
+  const handleSideToogle = () => {
+    setOpenSide(!openSide);
+  };
+  return (
+    <Box sx={appWrapper}>
+      <NavMenu handleSideToggle={handleSideToogle} />
+      <ContentArea open={openSide} />
+    </Box>
+  );
+};
